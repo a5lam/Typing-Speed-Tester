59f54200ec2fde3c51bc8d873073004c636c59d3
aslamshaikh524 <aslamshaikh524@gmail.com>
2020-07-15 20:49:51 -0400

    add timer and counting clock.

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = [0, 0, 0, 0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time){
+  if (time<= 9){
+    time= "0" + time;
+  }
+  return time;
+}
 
 // Run a standard minute/second/hundredths timer:
+function runtimer(){
+  let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+  theTimer.innerHTML = currentTime;
+  timer[3]++;
 
-
+  timer[0] = Math.floor((timer[3]/100)/60);
+  timer[1] = Math.floor((timer[3]/100) - (timer[0]*60));
+  timer[2] = Math.floor(timer[3] - (timer[1]*100) - (timer[0]*6000));
+}
 // Match the text entered with the provided text on the page:
 function spellCheck(){
   let textEntered = testArea.value;
@@ -20,6 +33,9 @@ function spellCheck(){
 // Start the timer:
 function start(){
   let textEnterdLength = testArea.value.length;
+  if (textEnterdLength ===0 ){
+    setInterval(runtimer, 10);
+  }
   console.log(textEnterdLength);
 }
 
